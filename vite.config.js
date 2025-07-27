ce260ef3b3b9bba38adbd05a37f19b4fbbc861ab
CCDevX <cavaleri.cristina.dev@gmail.com>
2025-07-27 16:51:44 +0200

    commit dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/",
-  root: "./src",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
-  server: {
-    hmr: false,
-  },
+export default defineConfig(({ command }) => {
+  const base = command === "build" ? "/vanilla-js-snake-canvas/" : "/";
+
+  return {
+    base,
+    root: "./src",
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+    },
+    server: {
+      hmr: false,
+    },
+  };
 });
